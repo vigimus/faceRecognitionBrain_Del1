db115ba63c14785af1e3e909c79e6e4756aa0484
vigimus <160164175+vigimus@users.noreply.github.com>
2024-12-17 19:18:29 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React { Component } from 'react';
+import Navigation from './Komponenter/Navigation/navigation';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+     { /*<Logo />
+           <ImageLinkForm/>
+           <FaceRecognition/>*/}
+    </div>
+  );
+}
+
+export default App;
